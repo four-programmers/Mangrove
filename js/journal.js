666b7c6566011f118787fa9000b89d2f28b8f537
kimks1234 <bisuflash1@naver.com>
2023-11-02 15:48:44 +0900

    231102 journal 추가

---
+++ js/journal.js
@@ -0,0 +1,8 @@
+const post1 = document.querySelector(".post1");
+const postAll = document.querySelectorAll(".post");
+const postItvw1 = document.querySelector(".post-interview1");
+console.log(`${postAll}`);
+post1.addEventListener("click", () => {
+  postItvw1.style.display = "block";
+  postAll.classList.add("active");
+});
