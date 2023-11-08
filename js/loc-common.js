f1435359abf3559c1dd88c99e47079b1eb4a2c17
You-been <ybcho97@naver.com>
2023-11-09 02:31:59 +0900

    location 파일 수정

--- js/loc-common.js
+++ js/loc-common.js
@@ -1,11 +1,11 @@
-const toggleBtn = document.querySelector(".toggle-btn");
-const toggleTxt = document.querySelector(".toggle-txt");
+const toggleBtn = document.querySelectorAll(".toggle-title");
+const toggleTxt = document.querySelectorAll(".faq-title");
 
-toggleBtn.addEventListener('click', evt => {
-  evt.preventDefault();
-  
-})
-closeBtn.addEventListener('click', evt => {
-  evt.preventDefault();
-
-})
+toggleBtn.forEach((e) => {
+  e.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    const target = evt.target;
+    toggleBtn.forEach((btn) => btn.classList.remove("active"));
+    target.parentNode.classList.toggle("active");
+  });
+});
