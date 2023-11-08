ba083452933c38e4527ef92a62a058cce1b49621
ksyoung00 <k_7700@naver.com>
2023-11-08 17:56:53 +0900

    231108 indes.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
-// index slide img
-const slideImg = document.querySelectorAll('.slide-wrapper .slide-img');
-
-let idx = 0;
-addEventListener('click', () => {
-  slideImg[idx].classList.remove('swipe');
-  idx++;
-  if (idx >= slideItems.length) idx = 0;
-  slideImg[idx].classList.add('swipe');
+// locations slide
+const mainSlide = new Swiper('.main-slide', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 1,
 });
 
-setInterval(() => {
-  .click();
-}, 2000);
+// locations-menu active
+const locationsMenuList = document.querySelectorAll('.locations-menu-list');
+
+locationsMenuList.forEach((list) => {
+  list.addEventListener("click", (e) => {
+    const target = e.target;
+    e.forEach((li) => {
+      li.classList.remove("active");
+      target.parentNode.classList.add("active")
+    });
+  });
+});
